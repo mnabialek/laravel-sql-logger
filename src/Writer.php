cb2d7149e8cc6952fd3c2f23157e782791b63a1c
Marcin Nabiałek <dev@nabialek.org>
2018-01-08 15:39:27 +0100

    Add Writer with tests

---
+++ src/Writer.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Mnabialek\LaravelSqlLogger;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Foundation\Application;
+use Mnabialek\LaravelSqlLogger\Objects\SqlQuery;
+
+class Writer
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * @var Formatter
+     */
+    private $formatter;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * Writer constructor.
+     *
+     * @param Application $app
+     * @param Formatter $formatter
+     * @param Config $config
+     */
+    public function __construct(Application $app, Formatter $formatter, Config $config)
+    {
+        $this->app = $app;
+        $this->formatter = $formatter;
+        $this->config = $config;
+    }
+
+    /**
+     * Save queries to log.
+     *
+     * @param SqlQuery $query
+     */
+    public function save(SqlQuery $query)
+    {
+        $this->createDirectoryIfNotExists($query->number());
+
+        $line = $this->formatter->getLine($query);
+
+        if ($this->config->logQueries()) {
+            $this->saveLine($line, $this->logName(), $this->shouldOverrideFile($query));
+        }
+
+        if ($this->shouldLogSlowQuery($query->time())) {
+            $this->saveLine($line, $this->slowLogName());
+        }
+    }
+
+    /**
+     * Create directory if it does not exist yet.
+     * 
+     * @param int $queryNumber
+     */
+    protected function createDirectoryIfNotExists($queryNumber)
+    {
+        if ($queryNumber == 1 && ! file_exists($directory = $this->directory())) {
+            mkdir($directory, 0777, true);
+        }
+    }
+
+    /**
+     * Get directory where file should be located.
+     *
+     * @return string
+     */
+    protected function directory()
+    {
+        return rtrim($this->config->logDirectory(), '\\/');
+    }
+
+    /**
+     * Get log name.
+     *
+     * @return string
+     */
+    protected function logName()
+    {
+        return Carbon::now()->format('Y-m-d') . $this->suffix() . '-log.sql';
+    }
+
+    /**
+     * Get slow log name.
+     *
+     * @return string
+     */
+    protected function slowLogName()
+    {
+        return Carbon::now()->format('Y-m-d') . $this->suffix() . '-slow-log.sql';
+    }
+
+    /**
+     * Get file suffix.
+     *
+     * @return string
+     */
+    protected function suffix()
+    {
+        return $this->config->separateConsoleLogs() && $this->app->runningInConsole() ? '-artisan' : '';
+    }
+
+    /**
+     * Verify whether slow query should be logged.
+     *
+     * @param float $execTime
+     *
+     * @return bool
+     */
+    protected function shouldLogSlowQuery($execTime)
+    {
+        return $this->config->logSlowQueries() && $execTime >= $this->config->slowLogTime();
+    }
+
+    /**
+     * Save data to log file.
+     *
+     * @param string $line
+     * @param string $fileName
+     * @param bool $override
+     */
+    protected function saveLine($line, $fileName, $override = false)
+    {
+        file_put_contents($this->directory() . DIRECTORY_SEPARATOR . $fileName,
+            $line, $override ? 0 : FILE_APPEND);
+    }
+
+    /**
+     * Verify whether file should be overridden.
+     *
+     * @param SqlQuery $query
+     *
+     * @return bool
+     */
+    private function shouldOverrideFile(SqlQuery $query)
+    {
+        return ($query->number() == 1 && $this->config->overrideFile());
+    }
+}
---
+++ tests/WriterTest.php
@@ -0,0 +1,357 @@
+<?php
+
+namespace Mnabialek\LaravelSqlLogger\Tests;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Filesystem\Filesystem;
+use Mnabialek\LaravelSqlLogger\Config;
+use Mnabialek\LaravelSqlLogger\Formatter;
+use Mnabialek\LaravelSqlLogger\Objects\SqlQuery;
+use Mnabialek\LaravelSqlLogger\Writer;
+use Mockery;
+
+class WriterTest extends UnitTestCase
+{
+    /**
+     * @var Application|\Mockery\Mock
+     */
+    private $app;
+
+    /**
+     * @var Formatter|\Mockery\Mock
+     */
+    private $formatter;
+
+    /**
+     * @var Config|\Mockery\Mock
+     */
+    private $config;
+
+    /**
+     * @var Writer
+     */
+    private $writer;
+
+    /**
+     * @var string
+     */
+    private $directory;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    protected function setUp()
+    {
+        $this->app = Mockery::mock(Application::class);
+        $this->formatter = Mockery::mock(Formatter::class);
+        $this->config = Mockery::mock(Config::class);
+        $this->writer = new Writer($this->app, $this->formatter, $this->config);
+        $this->directory = __DIR__ . '/test-dir/directory';
+        $this->filesystem = new Filesystem();
+    }
+
+    public function tearDown()
+    {
+        $this->filesystem->deleteDirectory($this->directory);
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function it_creates_directory_if_it_does_not_exist_for_1st_query()
+    {
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->once()->withNoArgs()->andReturn($this->directory);
+        $this->assertFileNotExists($this->directory);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertEmpty($this->filesystem->allFiles($this->directory));
+    }
+
+    /** @test */
+    public function it_does_not_create_directory_if_it_does_not_exist_for_2nd_query()
+    {
+        $query = new SqlQuery(2, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldNotReceive('logDirectory');
+        $this->assertFileNotExists($this->directory);
+        $this->writer->save($query);
+        $this->assertFileNotExists($this->directory);
+    }
+
+    /** @test */
+    public function it_creates_log_file()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('overrideFile')->once()->withNoArgs()->andReturn(false);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_appends_to_existing_log_file()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-log.sql';
+        mkdir($this->directory, 0777, true);
+        $initialContent = "Initial file content\n";
+        file_put_contents($expectedFileName, $initialContent);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('overrideFile')->once()->withNoArgs()->andReturn(false);
+        $this->assertFileExists($expectedFileName);
+        $this->writer->save($query);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($initialContent . $lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_replaces_current_file_content_for_1st_query_when_overriding_is_turned_on()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-log.sql';
+        mkdir($this->directory, 0777, true);
+        $initialContent = "Initial file content\n";
+        file_put_contents($expectedFileName, $initialContent);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('overrideFile')->once()->withNoArgs()->andReturn(true);
+        $this->assertFileExists($expectedFileName);
+        $this->writer->save($query);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_appends_to_current_file_content_for_2nd_query_when_overriding_is_turned_on()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-log.sql';
+        mkdir($this->directory, 0777, true);
+        $initialContent = "Initial file content\n";
+        file_put_contents($expectedFileName, $initialContent);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(2, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->once()->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldNotReceive('overrideFile');
+        $this->assertFileExists($expectedFileName);
+        $this->writer->save($query);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($initialContent . $lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_creates_artisan_log_file_when_console_log_should_be_logged_to_separate_file_when_running_in_console()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('overrideFile')->once()->withNoArgs()->andReturn(false);
+        $this->app->shouldReceive('runningInConsole')->once()->withNoArgs()->andReturn(true);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-artisan-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_creates_normal_log_file_when_console_log_should_be_logged_to_separate_file_when_not_running_in_console()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('overrideFile')->once()->withNoArgs()->andReturn(false);
+        $this->app->shouldReceive('runningInConsole')->once()->withNoArgs()->andReturn(false);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_adds_query_to_slow_log_when_its_greater_than_given_time()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample slow log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('slowLogTime')->once()->withNoArgs()->andReturn(5.22);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(false);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-slow-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_does_not_add_query_to_slow_log_when_its_lower_than_given_time()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample slow log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('slowLogTime')->once()->withNoArgs()->andReturn(5.42);
+        $this->config->shouldReceive('logDirectory')->once()->withNoArgs()->andReturn($this->directory);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertEmpty($this->filesystem->allFiles($this->directory));
+    }
+
+    /** @test */
+    public function it_creates_artisan_log_file_for_slow_log_when_console_log_should_be_logged_to_separate_file_when_running_in_console()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('slowLogTime')->once()->withNoArgs()->andReturn(5.33);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(true);
+        $this->app->shouldReceive('runningInConsole')->once()->withNoArgs()->andReturn(true);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-artisan-slow-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_creates_normal_log_file_for_slow_log_when_console_log_should_be_logged_to_separate_file_when_not_running_in_console()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('slowLogTime')->once()->withNoArgs()->andReturn(5.33);
+        $this->config->shouldReceive('logDirectory')->times(2)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->once()->withNoArgs()->andReturn(true);
+        $this->app->shouldReceive('runningInConsole')->once()->withNoArgs()->andReturn(false);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(1, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-slow-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+    }
+
+    /** @test */
+    public function it_creates_2_files_when_both_log_set_to_true()
+    {
+        $now = Carbon::parse('2015-02-03 06:41:31');
+        Carbon::setTestNow($now);
+
+        $lineContent = 'Sample log line';
+
+        $query = new SqlQuery(1, 'test', [], 5.41);
+        $this->formatter->shouldReceive('getLine')->once()->with($query)->andReturn($lineContent);
+        $this->config->shouldReceive('logQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('logSlowQueries')->once()->withNoArgs()->andReturn(true);
+        $this->config->shouldReceive('slowLogTime')->once()->withNoArgs()->andReturn(5.33);
+        $this->config->shouldReceive('logDirectory')->times(3)->withNoArgs()->andReturn($this->directory);
+        $this->config->shouldReceive('separateConsoleLogs')->times(2)->withNoArgs()->andReturn(false);
+        $this->config->shouldReceive('overrideFile')->once()->withNoArgs()->andReturn(false);
+        $this->writer->save($query);
+        $this->assertFileExists($this->directory);
+        $this->assertCount(2, $this->filesystem->allFiles($this->directory));
+        $expectedFileName = $this->directory . '/' . $now->toDateString() . '-log.sql';
+        $this->assertFileExists($expectedFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedFileName));
+        $expectedSlowFileName = $this->directory . '/' . $now->toDateString() . '-slow-log.sql';
+        $this->assertFileExists($expectedSlowFileName);
+        $this->assertSame($lineContent, file_get_contents($expectedSlowFileName));
+    }
+}
